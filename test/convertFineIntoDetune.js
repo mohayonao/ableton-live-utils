730d23335cfaaefef718875773f96a26ac793097
mohayonao <mohayonao@gmail.com>
2016-07-06 06:21:45 +0900

    replace mocha -> eater

--- test/convertFineIntoDetune.js
+++ test/convertFineIntoDetune.js
@@ -1,36 +1,35 @@
-var assert = require("power-assert");
-var convertFineIntoDetune = require("../convertFineIntoDetune");
+"use strict";
+
+const assert = require("power-assert");
+const convertFineIntoDetune = require("../convertFineIntoDetune");
 
 function closeTo(expected, actual, delta) {
   return Math.abs(expected - actual) <= delta;
 }
 
-describe("convertFineIntoDetune(fine: number): number", function() {
-  it("works", function() {
-    assert(convertFineIntoDetune(1000) === 1200);
-    assert(closeTo(convertFineIntoDetune(888), 1100, 1));
-    assert(closeTo(convertFineIntoDetune(782), 1000, 1));
-    assert(closeTo(convertFineIntoDetune(682), 900, 1));
-    assert(closeTo(convertFineIntoDetune(587), 800, 1));
-    assert(closeTo(convertFineIntoDetune(498), 700, 1));
-    assert(closeTo(convertFineIntoDetune(414), 600, 1));
-    assert(closeTo(convertFineIntoDetune(335), 500, 1));
-    assert(closeTo(convertFineIntoDetune(260), 400, 1));
-    assert(closeTo(convertFineIntoDetune(189), 300, 1));
-    assert(closeTo(convertFineIntoDetune(122), 200, 1));
-    assert(closeTo(convertFineIntoDetune(59), 100, 1));
-    assert(convertFineIntoDetune(0) === 0);
-    assert(closeTo(convertFineIntoDetune(-59), -100, 1));
-    assert(closeTo(convertFineIntoDetune(-122), -200, 1));
-    assert(closeTo(convertFineIntoDetune(-189), -300, 1));
-    assert(closeTo(convertFineIntoDetune(-260), -400, 1));
-    assert(closeTo(convertFineIntoDetune(-335), -500, 1));
-    assert(closeTo(convertFineIntoDetune(-414), -600, 1));
-    assert(closeTo(convertFineIntoDetune(-498), -700, 1));
-    assert(closeTo(convertFineIntoDetune(-587), -800, 1));
-    assert(closeTo(convertFineIntoDetune(-682), -900, 1));
-    assert(closeTo(convertFineIntoDetune(-782), -1000, 1));
-    assert(closeTo(convertFineIntoDetune(-888), -1100, 1));
-    assert(convertFineIntoDetune(-1000) === -1200);
-  });
-});
+assert(convertFineIntoDetune(1000) === 1200);
+assert(closeTo(convertFineIntoDetune(888), 1100, 1));
+assert(closeTo(convertFineIntoDetune(782), 1000, 1));
+assert(closeTo(convertFineIntoDetune(682), 900, 1));
+assert(closeTo(convertFineIntoDetune(587), 800, 1));
+assert(closeTo(convertFineIntoDetune(498), 700, 1));
+assert(closeTo(convertFineIntoDetune(414), 600, 1));
+assert(closeTo(convertFineIntoDetune(335), 500, 1));
+assert(closeTo(convertFineIntoDetune(260), 400, 1));
+assert(closeTo(convertFineIntoDetune(189), 300, 1));
+assert(closeTo(convertFineIntoDetune(122), 200, 1));
+assert(closeTo(convertFineIntoDetune(59), 100, 1));
+
+assert(convertFineIntoDetune(0) === 0);
+assert(closeTo(convertFineIntoDetune(-59), -100, 1));
+assert(closeTo(convertFineIntoDetune(-122), -200, 1));
+assert(closeTo(convertFineIntoDetune(-189), -300, 1));
+assert(closeTo(convertFineIntoDetune(-260), -400, 1));
+assert(closeTo(convertFineIntoDetune(-335), -500, 1));
+assert(closeTo(convertFineIntoDetune(-414), -600, 1));
+assert(closeTo(convertFineIntoDetune(-498), -700, 1));
+assert(closeTo(convertFineIntoDetune(-587), -800, 1));
+assert(closeTo(convertFineIntoDetune(-682), -900, 1));
+assert(closeTo(convertFineIntoDetune(-782), -1000, 1));
+assert(closeTo(convertFineIntoDetune(-888), -1100, 1));
+assert(convertFineIntoDetune(-1000) === -1200);
