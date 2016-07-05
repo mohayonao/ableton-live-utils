55f304723f4153c2076d9594b5331d3b3ee0e50f
mohayonao <mohayonao@gmail.com>
2016-07-06 06:24:35 +0900

    update coding style

--- convertFineIntoRatio.js
+++ convertFineIntoRatio.js
@@ -1,6 +1,15 @@
-module.exports = function(fine) {
+"use strict";
+
+/**
+ * convert to ratio from ableton-live operator's fine
+ * @param  {number} fine
+ * @return {number}
+ */
+function convertFineIntoRatio(fine) {
   if (fine < 0) {
     return 1 / (1 - fine / 1000);
   }
   return 1 + fine / 1000;
-};
+}
+
+module.exports = convertFineIntoRatio;
